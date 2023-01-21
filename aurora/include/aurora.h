31cbce04d20fa1f83db167d817889e2bedc4ba08
output <79727016+UntitledOutput@users.noreply.github.com>
2023-01-21 12:06:54 -0500

    aaiaiiaiaiaai

--- aurora/include/aurora.h
+++ aurora/include/aurora.h
@@ -14,7 +14,7 @@ typedef struct Scene {
     ModelData *modelData;
 } Scene;
 
-void Draw3D(Scene scene); // Draw 3D scene to window/screen
+void Draw3D(Scene scene, Camera3D camera); // Draw 3D scene to window/screen
 
 void LoadSceneFromJson(const char* path); // Get scene from JSON
 Scene CreateScene(); // Create scene from scratch
